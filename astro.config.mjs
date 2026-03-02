c941c95ac8b025af177a4cc734c65890e7789fc1
Paramita <freewill138>
2026-03-03 07:43:28 +0800

    fix: enable rehype-raw to render HTML in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import rehypeRaw from 'rehype-raw';
 
 export default defineConfig({
   site: 'https://quantmacro.vercel.app',
   integrations: [mdx(), tailwind()],
   markdown: {
+    rehypePlugins: [rehypeRaw],
     shikiConfig: {
       theme: 'github-dark'
     }
